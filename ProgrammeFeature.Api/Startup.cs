eeac074000cba5398659d9926f47521951642e67
sujeet kumar <sujeetkumar2010@live.com>
2020-08-29 20:04:04 +0530

    test commit

--- ProgrammeFeature.Api/Startup.cs
+++ ProgrammeFeature.Api/Startup.cs
@@ -39,6 +39,7 @@ namespace ProgrammeFeature.Api
             services.AddScoped<IProgrammeDetailsMaster, ProgrammeDetailsMasterService>();
 
 
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
