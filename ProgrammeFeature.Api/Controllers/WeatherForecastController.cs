84b5d6d7bf6d8d5fc4cbbbe2f5ebbd5b3153edc6
sujeet kumar <sujeetkumar2010@live.com>
2020-09-29 12:30:34 +0530

    sujeet

--- ProgrammeFeature.Api/Controllers/WeatherForecastController.cs
+++ ProgrammeFeature.Api/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace ProgrammeFeature.Api.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "sujeet", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
